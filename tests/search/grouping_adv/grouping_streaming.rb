c15c1a15f97aebad48f9760fc806fbd07d228ce9
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2022-04-05 14:36:35 +0200

    fixup! WIP

--- tests/search/grouping_adv/grouping_streaming.rb
+++ tests/search/grouping_adv/grouping_streaming.rb
@@ -45,5 +45,12 @@ class GroupingStreaming < StreamingSearchTest
     querytest_global_max
   end
 
+  def test_groups_for_default_value
+    set_owner("bjorncs")
+    deploy_app(singlenode_streaming_2storage(selfdir+"test.sd"))
+    start
+    feed_and_wait_for_docs('test', 2, :file => "#{selfdir}/default-values/docs.json")
+    querytest_groups_for_default_value
+  end
 
 end
